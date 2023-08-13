180d78b1e48dc6b89192854efe02e01d821a01b3
hari.js <109363771+hariFED@users.noreply.github.com>
2023-08-13 19:42:24 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/App.js", "./src/index.js", "./src/components/JobcardBox.js",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
